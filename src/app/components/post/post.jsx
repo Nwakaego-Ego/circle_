1efbf53773822035d34ed499d4cf0103e83e070b
Ego Nwaekpe <glorynwaekpe@gmail.com>
2025-02-16 21:02:09 +0100

    started working on the circle component

---
+++ src/app/components/post/post.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Image from "next/image";
+
+const Post = () => {
+  return (
+    <>
+      <div>
+        <Image src="/ego.png" alt="ego-image" width={50} height={50} />
+      </div>
+    </>
+  );
+};
+
+export default Post;
--- src/app/page.js
+++ src/app/page.js
@@ -2,18 +2,26 @@
 import Navbar from "./components/navbar/navbar";
 import Sidebar from "./components/sidebar/sidebar";
 import Hero from "./components/hero/hero";
+import Circle from "./components/circle/circle";
+import Post from "./components/post/post";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="page-container">
+      <div className="sidehero-container">
         <div>
           {" "}
           <Sidebar />
+          <Post />
         </div>
-        <div>
-          <Hero />{" "}
+        <div className="herocircle-container">
+          <div>
+            <Hero />{" "}
+          </div>
+          <div>
+            <Circle />
+          </div>
         </div>
       </div>
     </>
